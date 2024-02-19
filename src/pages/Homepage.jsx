3f29e33d964dc5d7cddded25897054b4c40c2433
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2024-02-19 17:07:39 -0300

    Create a navbar home page

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,35 +1,11 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+
+import NavbarHomePage from "../features/NavbarHomePage";
 
 const Homepage = () => {
   return (
     <div>
-      <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-600 md:text-4xl lg:text-4xl dark:text-white">
-        Recomenda Cursos Online
-      </h1>
-      <Link
-        className="font-medium text-xl text-blue-600 dark:text-blue-500 hover:underline"
-        to="/depilacao-profissional"
-      >
-        1 - Depilação Profissional
-      </Link>
-      <br />
-      <hr />
-      <Link
-        className="font-medium text-xl text-blue-600 dark:text-blue-500 hover:underline"
-        to="/sobrancelha-perfeita"
-      >
-        2 - Sobrancelha Perfeita
-      </Link>
-      <br />
-      <hr />
-      <Link
-        className="font-medium text-xl text-blue-600 dark:text-blue-500 hover:underline"
-        to="/viver-cilios"
-      >
-        3 - Viver de Cílios
-      </Link>
-      <br />
-      <hr />
+      <NavbarHomePage />
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        softBlue: "hsl(231, 69%, 60%)",
+        softRed: "hsl(0, 94%, 66%)",
+        grayishBlue: "hsl(229, 8%, 60%)",
+        veryDarkBlue: "hsl(229, 31%, 21%)",
+      },
+    },
   },
   plugins: [],
 };
